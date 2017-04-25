8e9fd1daa42e3e6743c99c86eaab61f43b492093
harisolaas <h.solaas1@gmail.com>
2017-04-25 12:55:09 -0300

    Arreglo logo facebook footer

--- elements/footer.php
+++ elements/footer.php
@@ -1,7 +1,7 @@
 <footer>
   <nav>
     <ul>
-      <li><a href="#"><span class="icon ion-social-facebook"></span></a></li>
+      <li><a href="#"><span class="ion-social-facebook"></span></a></li>
       <li><a href="#"><span class="ion-social-instagram"></span></a></li>
       <li><a href="#"><span class="ion-social-twitter"></span></a></li>
       <li><a href="#"><span class="ion-social-linkedin"></span></a></li>
