5ccf6c162d5712c47fef4cf96b76993eeae7e1ba
miguel <miguel_rusconi@hotmail.com>
2017-04-26 11:51:41 -0300

    Acomodo obsesion

--- elements/sign-up-controller.php
+++ elements/sign-up-controller.php
@@ -1,50 +1,9 @@
 <?php
 session_start();
 
-//var_dump($_POST);
+
 include 'validation.php';
-/*echo "<br>";
-echo $errorFirstName;
-echo "<br>";
-echo $errorLastName;
-echo "<br>";
-echo $errorPass;
-echo "<br>";
-echo $errorEmail;
-echo "<br>";
-/*
-$rawUserData = [];
-$rawUserData = $_POST;
-var_dump($rawUserData);
-echo "<br>";
-echo "<br>";
-$users = file_get_contents('users.json');
-var_dump($users);
-echo "<br>";
-echo "<br>";
-$users = json_decode($users, true);
-var_dump($users);
-echo "<br>";
-echo "<br>";
-$users[$_POST['email']] = $rawUserData;
-var_dump($users);
-echo "<br>";
-echo "<br>";
-$users = json_encode($users, true);
-var_dump($users);
-echo "<br>";
-echo "<br>";
-file_put_contents('users.json', $users);*/
-/*
-function newUser(){
-    $rawUserData = [];
-    $rawUserData = $_POST;
-    $users = file_get_contents('users.json');
-    $users = json_decode($users, true);
-    $users[$_POST['email']] = $rawUserData;
-    $users = json_encode($users, true);
-    file_put_contents('users.json', $users);
-}*/
+
 
 if (($errorEmail == false) && ($errorFirstName == false) && ($errorLastName == false) && ($errorPass == false)) {
     $rawUserData = [];
@@ -76,8 +35,5 @@ if (($errorEmail == false) && ($errorFirstName == false) && ($errorLastName == f
 
 }
 
-//} //else {
-    // redireccion devuelta a sing up con mas todos los errores listos para poner en el formulario . inicializar $_session para que me tome las variables de los errores.
-//}
 
  ?>
--- elements/validation.php
+++ elements/validation.php
@@ -7,31 +7,30 @@ $errorPass = '';
 $errorEmail= '';
 
 
-/*$resultado = preg_replace("/[^a-z0-9 ná-ú]+/", "", $nombre);*/
+
 
 
 
 
         // VALIDACION NAME < 30 caracteres
 $firstName = trim($_POST["first-name"]);
-// NECESITO AVERIGUAR COMO APLICAR EN FILTER_SANITIZE_STRING
-$firstName = filter_var($firstName, FILTER_SANITIZE_STRING);
 if(strlen($firstName) > 30){
     $errorFirstName = 'El nombre es demasiado largo';
 }
+
 // VALIDACION LAST NAME
 $lastName = trim($_POST["last-name"]);
-// NECESITO AVERIGUAR COMO APLICAR EN FILTER_SANITIZE_STRING
-$lastName = filter_var($lastName, FILTER_SANITIZE_STRING);
 if(strlen($lastName) > 30){
     $errorLastName = 'El apellido es demasiado largo';
 }
+
         // VALIDACION PASSWORD vs CONFIRM_PASSWORD
         $pass = $_POST['pass'];
         $confirmPass = $_POST['confirm-pass'];
 if(isset($_POST['pass']) && isset($_POST['confirm-pass'])){
     if($_POST['pass'] !== $_POST['confirm-pass']){
         $errorPass = 'Los Passwords no coinciden';
+
 
         // VALIDACION LONGITUD DEL PASSWORD + de 6 caracteres
     } elseif(strlen($_POST['pass']) < 6){
