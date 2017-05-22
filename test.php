d9e057d7e3783bade4abb2e32cfaacdf018f4706
harisolaas <h.solaas1@gmail.com>
2017-05-22 11:46:47 -0300

    users class

--- test.php
+++ test.php
@@ -1,2 +1,4 @@
 <?php
-header('Location: /proyecto-integrador/paginas/home.php');
+require 'helpers/class.user.php';
+$user = new User('hari@solaas.com', 'Hari', 'Olaiazola', '123456', '123456');
+var_dump($user);
