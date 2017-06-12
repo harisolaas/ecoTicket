1f21a2e1fffffc6cb6b8f769745c1d91914ab029
miguel <miguel_rusconi@hotmail.com>
2017-06-12 10:59:10 -0300

    esqueleto clase SQL users repo

---
+++ helpers/classes/repo/class.MYSQLUsersRepo.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'class.UsersRepo.php';
+class MYSQLUsersRepo extends UsersRepo{
+
+
+    public function __construct(){
+        PDO CONECTION
+    }
+
+    public function getUserByMail(){
+
+    }
+
+    public function getUserField(){
+
+    }
+
+    public function saveUser(){
+
+    }
+}
