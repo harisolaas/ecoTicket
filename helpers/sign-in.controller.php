70694c955a3730f8470c3621ba194c48c0875228
harisolaas <h.solaas1@gmail.com>
2017-06-13 19:47:06 -0300

    Lo mismo

---
+++ helpers/sign-in.controller.php
@@ -0,0 +1,30 @@
+<?php
+
+session_start();
+
+require_once 'classes/repo/class.JSONUsersRepo.php';
+
+require_once 'classes/validator/class.LogInValidator.php';
+
+require_once 'classes/auth/class.PhCustomAuth.php';
+
+$repo = new JSONUsersRepo();
+
+$validator = new LogInValidator();
+
+$validator = $validator->validate($_POST, $repo);
+
+if (!$validator) {
+    $auth = new PhCustomAuth();
+    $auth->logIn($_POST['mail']);
+    if (isset($_POST['rememberMe'])) {
+        $auth->rememberMe($repo);
+    }else {
+        $auth->unsetRememberMe();
+    }
+
+    header('Location: '.$_SERVER['DOCUMENT_ROOT'].'/ecoTicket');
+}else {
+    $_SESSION['errors'] = $validator;
+    header('Location: '.$_SERVER['DOCUMENT_ROOT'].'/ecoTicket/sign-in.php');
+}
